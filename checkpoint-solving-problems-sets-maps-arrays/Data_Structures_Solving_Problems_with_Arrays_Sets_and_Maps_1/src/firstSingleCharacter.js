e3d5e6457cb84d51762ee1fd7f98ff14f3f1545a
Reilly Thompson <thompson.reilly@yahoo.com>
2021-09-02 16:51:12 -0700

    updated solving problems assignment

--- checkpoint-solving-problems-sets-maps-arrays/Data_Structures_Solving_Problems_with_Arrays_Sets_and_Maps_1/src/firstSingleCharacter.js
+++ checkpoint-solving-problems-sets-maps-arrays/Data_Structures_Solving_Problems_with_Arrays_Sets_and_Maps_1/src/firstSingleCharacter.js
@@ -2,6 +2,33 @@
  * Return the first character in the string that occurs only once.
  * @param {string} word the string to be analysed
  */
-function firstSingleCharacter(word) {}
+function firstSingleCharacter(word) {
+  const wordArray = word.toLowerCase().split("");
+  console.log("word array: ", wordArray);
+  const wordMap = new Map();
+  console.log("word map: ", wordMap);
+
+  wordArray.forEach((letter) => {
+    if (wordMap.has(letter)) {
+      wordMap.set(letter, wordMap.get(letter) + 1);
+    } else {
+      wordMap.set(letter, 1);
+    }
+  });
+  console.log("word map: ", wordMap);
+
+  let uniqueLetter = null;
+
+  wordMap.forEach((value, letter) => {
+    console.log("number of occurences: ", value);
+    if (value === 1) {
+      if (uniqueLetter === null) {
+        uniqueLetter = letter;
+      }
+    }
+  });
+  return uniqueLetter;
+  console.log("first single character: ", uniqueLetter);
+}
 
 module.exports = firstSingleCharacter;
--- checkpoint-solving-problems-sets-maps-arrays/Data_Structures_Solving_Problems_with_Arrays_Sets_and_Maps_1/src/permutationPalindrome.js
+++ checkpoint-solving-problems-sets-maps-arrays/Data_Structures_Solving_Problems_with_Arrays_Sets_and_Maps_1/src/permutationPalindrome.js
@@ -1,7 +1,40 @@
-/**
- * Return true if some permutation of this word is a palindrome
- * @param {string} word The word to check
- */
-function permutationPalindrome(word) {}
+function permutationPalindrome(word) {
+  const letters = word.toLowerCase().split("");
+  const wordMap = new Map();
+
+  letters.forEach((letter) => {
+    if (wordMap.has(letter)) {
+      wordMap.set(letter, wordMap.get(letter) + 1);
+    } else {
+      wordMap.set(letter, 1);
+    }
+  });
+  let count = 0;
+
+  wordMap.forEach((value, letter) => {
+    //> if the letter has an odd count
+    if (value % 2 === 1) {
+      count += 1;
+    }
+  });
+
+  //> word is not a palindrome if there is more than one
+  //> letter with an odd number of occurences
+  if (count > 1) {
+    return false;
+  }
+
+  //> word is not a palindrome if it has one odd letter
+  //> but an even number of letters
+  if (count === 1 && letters.length % 2 === 0) {
+    return false;
+  }
+
+  //> word is a palindrome if it either 1) has an even number of
+  //> letters (each with an even number of occurences), or has an
+  //> odd number of letters, but only one letter than has an odd
+  //> number of occurences
+  return true;
+}
 
 module.exports = permutationPalindrome;
