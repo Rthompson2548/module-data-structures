61bedf67fefaf5437397509a06c4c01f75602900
Reilly Thompson <thompson.reilly@yahoo.com>
2021-09-17 16:06:29 -0700

    updated setOperations.js from sets and maps assignment

--- checkpoint-sets-and-maps/Data_Structures_Sets_and_Maps_1/src/equalArrays.js
+++ checkpoint-sets-and-maps/Data_Structures_Sets_and_Maps_1/src/equalArrays.js
@@ -2,14 +2,7 @@
  * return true if two arrays are equal, false otherwise
  */
 
-function isEqual(a1, a2) {
-  console.log("a1: ", a1);
-  console.log("a2: ", a2);
-
-  if (a1.equals(a2)) {
-    return true;
-  }
-  return false;
+function isEqual(a, b) {
 }
 
 module.exports = isEqual;
--- checkpoint-sets-and-maps/Data_Structures_Sets_and_Maps_1/src/setOperations.js
+++ checkpoint-sets-and-maps/Data_Structures_Sets_and_Maps_1/src/setOperations.js
@@ -1,29 +1,55 @@
 /**
- * return the union of two sets
+ * write a function that accepts two sets and returns
+ * the union of the two sets.
  */
- function union(s1, s2) {
-    let union = new Set([...s1, ...s2])
-    console.log("union: ", union)
-    return union
+function union(s1, s2) {
+  // creates a set that include the values from s1
+  const union = new Set(s1);
+
+  // iterates through values of s2 and appends them to 'union' if not already included
+  for (const value of s2) {
+    union.add(value);
   }
-  
-  /**
-   * return the intersection of two sets
-   */
-  function intersect(s1, s2) {
-    let intersection = new Set([...s1].filter(x => s2.has(x)))
-    console.log("intersection: ", intersection)
-    return intersection
-  }
-  
-  /**
-   * return the difference of two sets
-   */
-  function difference(s1, s2) {
-    let difference = new Set([...s1].filter(x => !s2.has(x)))
-    console.log("difference: ", difference)
-    return difference
-  }
-  
-  module.exports = { union, intersect, difference };
-  
+
+  return union;
+  console.log("union of s1 and s2:", union);
+}
+
+/**
+ * other possible solution for union using spread operator
+ */
+function union(s1, s2) {
+  console.log("s1:", s1, "s2:", s2);
+
+  // creates a set using spread operator with all unique values from both sets
+  const union = new Set([...s1, ...s2]);
+  return union;
+  console.log("union:", union);
+}
+
+/**
+ * return the intersection of two sets
+ * the intersection of two sets are the elements that belong to both sets.
+ */
+function intersect(s1, s2) {
+  // creates set using s1 vals, and uses filter() method to only
+  // add the vals also included in s2
+  let intersection = new Set([...s1].filter((val) => s2.has(val)));
+  console.log("intersection:", intersection);
+  return intersection;
+}
+
+/**
+ * return the difference of two sets
+ * the difference between two sets a and b are
+ * all the elements that belong to a but not b
+ */
+function difference(s1, s2) {
+  // creates a set that filters through s2 and only adds values
+  // that are not included in s2
+  let difference = new Set([...s1].filter((val) => !s2.has(val)));
+  console.log("difference :", difference);
+  return difference;
+}
+
+module.exports = { union, intersect, difference };
