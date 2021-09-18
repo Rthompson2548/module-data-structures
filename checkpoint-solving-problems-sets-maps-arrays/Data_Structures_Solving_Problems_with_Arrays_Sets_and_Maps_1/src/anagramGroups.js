e8854d6cc3e52d580141ebc7ebfc8d8311603e4e
Reilly Thompson <thompson.reilly@yahoo.com>
2021-09-17 20:20:08 -0700

    updated anagram.js file from sets/maps/arrays assignment with log statements to

--- checkpoint-solving-problems-sets-maps-arrays/Data_Structures_Solving_Problems_with_Arrays_Sets_and_Maps_1/src/anagramGroups.js
+++ checkpoint-solving-problems-sets-maps-arrays/Data_Structures_Solving_Problems_with_Arrays_Sets_and_Maps_1/src/anagramGroups.js
@@ -2,26 +2,6 @@
  * Return a grouping of words by whether they are anagrams of each other or not
  * @param {array} words to be grouped
  */
-function anagramGroups(words) {
-  const results = new Map();
-
-  words.forEach((word) => {
-    const lowerCaseWord = word.toLowerCase().split("").sort().join("");
-    console.log("lower case word: ", lowerCaseWord);
-
-    const group = results.get(lowerCaseWord);
-    console.log("word group: ", group);
-
-    results.set(lowerCaseWord, group ? [...group, word] : [word]);
-  });
-
-  const anagram = Object.fromEntries(results);
-  console.log("anagram: ", anagram);
-
-  const sameLetters = Object.values(anagram);
-
-  console.log("words with same letters: ", sameLetters);
-  return sameLetters;
-}
+function anagramGroups(words) {}
 
 module.exports = anagramGroups;
