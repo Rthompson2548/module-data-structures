0cf0e3755ae47fd48b7f42d2fc74055500ddef5b
Reilly Thompson <thompson.reilly@yahoo.com>
2021-09-18 14:34:46 -0700

    updated permutationPalendrome.js file from sets/maps/arrays assignments with log statements

--- checkpoint-arrays/Data_Structures_Arrays_1/src/minimumAndMaximum.js
+++ checkpoint-arrays/Data_Structures_Arrays_1/src/minimumAndMaximum.js
@@ -1,12 +1,26 @@
 /**
  * Implement an algorithm that sorts the array then returns the minimum and maximum
  */
- function minimumAndMaximumSort(numbers) {}
+function minimumAndMaximumIterate(numbers) {
+  if (numbers.length >= 1) {
+    let min = numbers[0];
+    let max = numbers[0];
+    numbers.forEach((number) => {
+      if (number < min) {
+        min = number;
+      }
+      if (number > max) {
+        max = number;
+      }
+    });
+    return [min, max];
+  }
+  return [];
+}
 
- /**
-  * Implement an algorithm that uses iteration to find the minimum and maximum
-  */
- function minimumAndMaximumIterate(numbers) {}
- 
- module.exports = { minimumAndMaximumIterate, minimumAndMaximumSort };
- 
+/**
+ * Implement an algorithm that uses iteration to find the minimum and maximum
+ */
+function minimumAndMaximumIterate(numbers) {}
+
+module.exports = { minimumAndMaximumIterate, minimumAndMaximumSort };
